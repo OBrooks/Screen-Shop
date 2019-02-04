0a15f6e860b31cbdeb2319687c82216dc83fd6fb
OBrooks <mr.willh@gmail.com>
2019-02-04 18:20:52 +0100

    remove some lines of validates in User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   
-  validates_presence_of :nickname, uniqueness: true, on: :create
-  validates_presence_of :birthdate, on: :create
-  validates_presence_of :gender, on: :create
-  validates_presence_of :town, on: :create
-  validates_presence_of :first_name, on: :create
-  validates_presence_of :last_name, on: :create
+  validates_presence_of :email, uniqueness: true, on: :create
 end
