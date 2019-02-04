bfb6ed4e003788f5152854683470aa4c07ce8a2b
OBrooks <mr.willh@gmail.com>
2019-02-04 18:19:43 +0100

    add jquery to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  validates_presence_of :nickname, uniqueness: true, on: :create
+  validates_presence_of :birthdate, on: :create
+  validates_presence_of :gender, on: :create
+  validates_presence_of :town, on: :create
+  validates_presence_of :first_name, on: :create
+  validates_presence_of :last_name, on: :create
 end
