12e3e7d2673f0ea6ad045f74afc89fcc270e5e46
OBrooks <mr.willh@gmail.com>
2019-05-27 19:03:08 +0200

    fix confirmation controller notice v.2

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,6 +4,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     def after_confirmation_path_for(resource_name, resource)
         sign_in(resource)
         # In case you want to sign in the user
-        {redirect_to root_path, notice: "Vous êtes maintenant inscrit !" }
+        redirect_to root_path
+        flash[:notice] = "Vous êtes maintenant inscrit !"
     end
 end
